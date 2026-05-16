875202cd94e8563c48c5e036977d6db7dbf26123
matheus00M7 <m7developer01@gmail.com>
2026-05-16 20:08:12 -0300

    resolvi alguns bugs do menu

--- src/menu.c
+++ src/menu.c
@@ -7,9 +7,10 @@ void exibirMenu(){
     printf("=====================================\n");
     printf("Escolha a opcao desejada: \n");
     printf("1- Consulte o saldo\n");
-    printf("2- Realize o saldo\n");
-    printf("3- Realize seu depósito\n");
-    printf("Ver o estrato bancário\n");
+    printf("2- Realize o saque\n");
+    printf("3- Realize seu deposito\n");
+    printf("4- Ver o extrato bancario\n");
     printf("0- Sair\n");
     printf("=====================================\n");
+    printf("Opcao: ");
 }
