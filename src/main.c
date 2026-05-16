91cbdaea5f36cb86b91edf07e781a9155fb1ce6b
matheus00M7 <m7developer01@gmail.com>
2026-05-16 18:08:19 -0300

    Adiciona estrutura inicial do projeto ATM

---
+++ src/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "../include/menu.h"
+#include "../include/operacoes.h"
+#include "../include/extrato.h"
+#include "../include/util.h"
+
+/*
+    Projeto Integrador - Sistema ATM em C
+
+    Integrantes:
+    Jun Uehara - RM: 570537
+    Matheus Martins Lacerda - RM: 570843
+
+    Arquivo principal do sistema.
+    Aqui fica o loop do menu e o switch-case que chama as funcoes.
+*/
+
+int main() {
+    int opcao = -1;
+    float saldo = 500.00;
+
+    char tipos[TAM_EXTRATO][20];
+    float valores[TAM_EXTRATO];
+    int totalOperacoes = 0;
+
+    do {
+        limparTela();
+        exibirMenu();
+
+        if (scanf("%d", &opcao) != 1) {
+            printf("\nEntrada invalida! Digite apenas numeros.\n");
+            limparBuffer();
+            pausarTela();
+            continue;
+        }
+
+        limparBuffer();
+
+        switch (opcao) {
+            case 1:
+                consultarSaldo(saldo);
+                break;
+
+            case 2:
+                saldo = realizarSaque(saldo, tipos, valores, &totalOperacoes);
+                break;
+
+            case 3:
+                saldo = realizarDeposito(saldo, tipos, valores, &totalOperacoes);
+                break;
+
+            case 4:
+                mostrarExtrato(tipos, valores, totalOperacoes, saldo);
+                break;
+
+            case 0:
+                printf("\nEncerrando o sistema...\n");
+                printf("Obrigado por usar o ATM!\n");
+                break;
+
+            default:
+                printf("\nOpcao invalida! Escolha uma opcao do menu.\n");
+                pausarTela();
+                break;
+        }
+
+    } while (opcao != 0);
+
+    return 0;
+}
