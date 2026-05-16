921cc4304a13e3323beb51c73e8526a4dcf516aa
JunUehara56 <jun.uehara.br@gmail.com>
2026-05-16 18:33:48 -0300

    Adicionado a interface do menu

---
+++ src/menu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../include/menu.h"
+
+void exibirMenu(){
+    printf("=====================================\n");
+    printf("          CAIXA ELETRONICO ATM       \n");
+    printf("=====================================\n");
+    printf("Escolha a opcao desejada: \n");
+    printf("1- Consulte o saldo\n");
+    printf("2- Realize o saldo\n");
+    printf("3- Realize seu depósito\n");
+    printf("Ver o estrato bancário\n");
+    printf("0- Sair\n");
+    printf("=====================================\n");
+}
